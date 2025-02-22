1a7c820014221f030b52b594d40fc34910e855bd
ak68ak68 <303760886@qq.com>
2025-02-22 14:13:58 +0800

    蓝牙连接

--- esp32_arduino_collection/servo_remote_esp32blue/servo_remote_esp32blue/servo_remote_esp32blue.ino
+++ esp32_arduino_collection/servo_remote_esp32blue/servo_remote_esp32blue/servo_remote_esp32blue.ino
@@ -7,63 +7,66 @@
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-// 定义指示灯连接的引脚
+// 定义 LED 连接的引脚
 const int ledPin = 2;
 
 // 全局变量
 BLECharacteristic *pCharacteristic;
 bool deviceConnected = false;
 
-// 自定义回调类，用于处理蓝牙连接和断开事件
+// 自定义回调类，处理蓝牙连接和断开事件
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
       deviceConnected = true;
+      Serial.println("Client connected!");
     };
 
     void onDisconnect(BLEServer* pServer) {
       deviceConnected = false;
+      Serial.println("Client disconnected!");
     }
 };
 
-// 自定义回调类，用于处理特征值的写入事件
+// 自定义回调类，处理特征值的写入事件
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
-      // 获取特征值并转换为 std::string
-      const char* valueCStr = pCharacteristic->getValue().c_str();
-      std::string value = std::string(valueCStr);
+        // 将 String 类型转换为 const char* 类型
+        const char* valueCStr = pCharacteristic->getValue().c_str();
+        // 使用 std::string 的构造函数将 const char* 转换为 std::string 类型
+        std::string value = std::string(valueCStr);
 
-      if (value.length() > 0) {
-        Serial.println("Received Value: " + String(value.c_str()));
+        if (value.length() > 0) {
+            Serial.println("Received Value: " + String(value.c_str()));
 
-        // 根据接收到的值控制指示灯
-        if (value[0] == '1') {
-          digitalWrite(ledPin, HIGH);
-        } else if (value[0] == '0') {
-          digitalWrite(ledPin, LOW);
+            // 根据接收到的值控制 LED 灯
+            if (value[0] == '1') {
+                digitalWrite(ledPin, HIGH);
+            } else if (value[0] == '0') {
+                digitalWrite(ledPin, LOW);
+            }
         }
-      }
     }
 };
 
 void setup() {
-  // 初始化串口通信
+  // 初始化串口通信，用于调试信息输出
   Serial.begin(115200);
 
-  // 初始化指示灯引脚
+  // 初始化 LED 引脚
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
 
-  // 创建蓝牙设备
+  // 初始化 BLE 设备
   BLEDevice::init("ESP32_BLE_LED");
 
-  // 创建蓝牙服务器
+  // 创建 BLE 服务器
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
-  // 创建蓝牙服务
+  // 创建 BLE 服务
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
-  // 创建蓝牙特征
+  // 创建 BLE 特征
   pCharacteristic = pService->createCharacteristic(
                       CHARACTERISTIC_UUID,
                       BLECharacteristic::PROPERTY_READ |
